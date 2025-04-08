705f195d73efd7ddf1e87288441f09cdc989f003
uesm016 <uesm016@e-triad.co.jp>
2025-04-08 13:50:26 +0900

    add navigator.clipboardが使えないときの回避策を追加

--- src/CodeWithCopyButton.tsx
+++ src/CodeWithCopyButton.tsx
@@ -4,7 +4,16 @@ import './CodeWithCopyButton.css';
 
 const CopyButton = ({ text }: { text: string }): JSX.Element => {
   const clickHandler = async() => {
-    await navigator.clipboard.writeText(text);
+    if (navigator.clipboard) {
+      await navigator.clipboard.writeText(text);
+    } else {
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+    }
   };
 
   return (
